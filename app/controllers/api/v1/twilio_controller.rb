ca96e106f79cf58d39d453086f141b0c33c5030a
jwsharpe <jameswrightsharpe@gmail.com>
2023-08-07 03:14:14 -0400

    webhook

--- app/controllers/api/v1/twilio_controller.rb
+++ app/controllers/api/v1/twilio_controller.rb
@@ -2,12 +2,8 @@
 
 class Api::V1::TwilioController < ApplicationController
   skip_before_action :verify_authenticity_token, raise: false
-  before_action :authenticate_devise_api_token!
 
   def webhook
-    # Process Twilio webhook notification here
-    # Update message status or perform other actions
-    put "Webhook recieved"
-    render plain: "Webhook received", status: :ok
+    render json: { message: "Webhook received" }, status: :ok
   end
 end
