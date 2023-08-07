a4c3d14e8e0e2cf74077e42eb1a826ab35b544ff
jwsharpe <jameswrightsharpe@gmail.com>
2023-08-07 04:10:42 -0400

    webhook

--- app/controllers/api/v1/twilio_controller.rb
+++ app/controllers/api/v1/twilio_controller.rb
@@ -4,10 +4,9 @@ class Api::V1::TwilioController < ApplicationController
   skip_before_action :verify_authenticity_token, raise: false
 
   def webhook
-    # Search for the most recent message with matching "to" phone number and "body"
-    recent_message = Message.where(phone_number: params[:to], body: params[:body]).order(created_at: :desc).first
+    recent_message = Message.where(phone_number: params[:To]).order(created_at: :desc).first
 
-    if recent_message.present?
+    if recent_message.present? and params[:SmsStatus] == "sent"
       recent_message.update(status: "SENT")
 
       render json: { message: "Webhook received, matching messages found, and status updated" }, status: :ok
